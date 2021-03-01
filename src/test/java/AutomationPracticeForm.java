ea5b901da70a45653a9ad0266942fb1b6bfb21d7
Dmitriy Avramenko <dmitriy.avramenko@nordigy.ru>
2021-03-01 15:37:11 +0300

    added new negative test

--- src/test/java/AutomationPracticeForm.java
+++ src/test/java/AutomationPracticeForm.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import steps.BaseSteps;
 
@@ -5,6 +6,23 @@ public class AutomationPracticeForm extends TestBase{
 
     @Test
     void fillFormTest() {
+        BaseSteps steps = new BaseSteps();
+        steps.openPage();
+        steps.fillUserFio();
+        steps.choseGender();
+        steps.chooseDate();
+        steps.chooseSubjects();
+        steps.chooseHobbies();
+        steps.uploadFile();
+        steps.fillAddress();
+        steps.subbmitBtn();
+
+        steps.checkRegisterForm();
+    }
+
+    @Test
+    @Tag("negative")
+    void fillFormNegativeTest() {
         BaseSteps steps = new BaseSteps();
         steps.openPage();
         steps.fillUserFio();
